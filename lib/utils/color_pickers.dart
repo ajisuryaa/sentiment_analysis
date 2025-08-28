4b5f09b191b7865bd979073bb0bdefba78e65eb6
ajisuryaa <ajisuryaahmadi@gmail.com>
2025-08-28 17:51:42 +0700

    add new color

--- lib/utils/color_pickers.dart
+++ lib/utils/color_pickers.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class ColorPickers {
   static const Color mainColor = Color(0xFF004894);
+  static const Color buttonColor = Color(0xFF4F8EF7);
+  static const Color shadowButton = Color(0x334F8EF7);
 }
