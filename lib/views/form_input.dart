75df60e89ab4f4b4d9db9be474c22a17e95f695f
ajisuryaa <ajisuryaahmadi@gmail.com>
2025-08-29 00:05:50 +0700

    fix layout and call cubit counter text

--- lib/views/form_input.dart
+++ lib/views/form_input.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sentiment_analysis/logic/cubit/count_text_cubit.dart';
 import 'package:sentiment_analysis/utils/color_pickers.dart';
 
 class FormInput extends StatefulWidget {
@@ -27,25 +29,33 @@ class _FormInputState extends State<FormInput> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => TextLengthCubit(),
+        child: Container(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                controller: _controller,
-                maxLines: null,
-                minLines: 5,
-                maxLength: 300,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Masukkan pesan Anda',
-                  hintText: 'Tulis pesan (maksimal 300 karakter)...',
-                  alignLabelWithHint: true,
-                ),
-                keyboardType: TextInputType.multiline,
+              BlocBuilder<TextLengthCubit, int>(
+                builder: (context, state) {
+                  return TextField(
+                    controller: _controller,
+                    maxLines: null,
+                    minLines: 5,
+                    maxLength: 300,
+                    onChanged: (value) {
+                      context.read<TextLengthCubit>().updateText(value);
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Masukkan pesan Anda',
+                      hintText: 'Tulis pesan (maksimal 300 karakter)...',
+                      alignLabelWithHint: true,
+                    ),
+                    keyboardType: TextInputType.multiline,
+                  );
+                },
               ),
               const SizedBox(height: 24),
               Row(
@@ -62,38 +72,47 @@ class _FormInputState extends State<FormInput> {
                           horizontal: 20, vertical: 14),
                     ),
                   ),
-                  ElevatedButton.icon(
-                    onPressed: _handleSubmit,
-                    icon: const Icon(Icons.send_rounded, color: Colors.white),
-                    label: const Text(
-                      'Submit',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        letterSpacing: 0.5,
-                        color: Colors.white,
-                        fontFamily: 'Roboto',
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: ColorPickers.buttonColor,
-                      foregroundColor: Colors.white,
-                      elevation: 6,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 28,
-                        vertical: 16,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(14),
-                      ),
-                      shadowColor: ColorPickers.shadowButton,
-                      textStyle: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        fontFamily: 'Roboto',
-                      ),
-                    ),
-                  ),
+                  BlocBuilder<TextLengthCubit, int>(
+                    builder: (context, state) {
+                      if (state > 0) {
+                        return ElevatedButton.icon(
+                          onPressed: _handleSubmit,
+                          icon:
+                              const Icon(Icons.analytics, color: Colors.white),
+                          label: const Text(
+                            'Start Analyze',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              letterSpacing: 0.5,
+                              color: Colors.white,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: ColorPickers.buttonColor,
+                            foregroundColor: Colors.white,
+                            elevation: 6,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 28,
+                              vertical: 16,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(14),
+                            ),
+                            shadowColor: ColorPickers.shadowButton,
+                            textStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        );
+                      } else {
+                        return Container();
+                      }
+                    },
+                  )
                 ],
               ),
             ],
