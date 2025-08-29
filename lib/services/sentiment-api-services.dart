9602c6a7ea9a7ced1a1b7582517746c53fdf343e
ajisuryaa <ajisuryaahmadi@gmail.com>
2025-08-29 07:33:52 +0700

    create integration api services

--- lib/services/sentiment-api-services.dart
+++ lib/services/sentiment-api-services.dart
@@ -2,25 +2,31 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class SentimentApiServices {
-  Future<void> analyzeSentiment(String text) async {
+  Future<dynamic> analyzeSentiment(String text) async {
     final url = Uri.parse("https://api.tabularis.ai/");
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: jsonEncode({
+          "text": text,
+          "return_all_scores": true,
+        }),
+      );
 
-    final response = await http.post(
-      url,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: jsonEncode({
-        "text": text,
-        "return_all_scores": false,
-      }),
-    );
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      print("Response: $data");
-    } else {
-      print("Error: ${response.statusCode} - ${response.body}");
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        print("Response: $data");
+        return data;
+      } else {
+        print("Error: ${response.statusCode} - ${response.body}");
+        throw Exception("Failed to analyze sentiment: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Exception occurred: $e");
+      throw Exception("Error analyzing sentiment: $e");
     }
   }
 }
