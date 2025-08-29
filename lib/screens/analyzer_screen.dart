171af80a90502dc3246ff77742a48ba51abfb0f3
ajisuryaa <ajisuryaahmadi@gmail.com>
2025-08-29 07:51:41 +0700

    create analyzer screen

---
+++ lib/screens/analyzer_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sentiment_analysis/logic/bloc/analyzer_sentiment/analyzer_sentiment_bloc.dart';
+import 'package:sentiment_analysis/logic/cubit/count_text_cubit.dart';
+import 'package:sentiment_analysis/views/form_input.dart';
+
+class AnalyzerScreen extends StatefulWidget {
+  const AnalyzerScreen({super.key});
+
+  @override
+  State<AnalyzerScreen> createState() => _FormInputState();
+}
+
+class _FormInputState extends State<AnalyzerScreen> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => TextLengthCubit(),
+          ),
+          BlocProvider(
+            create: (context) => SentimentAnalyzerBloc(),
+          ),
+        ],
+        child: BlocBuilder<SentimentAnalyzerBloc, SentimentAnalyzerState>(
+          builder: (context, state) {
+            if (state is SentimentAnalyzerInitial) {
+              return FormInput(
+                textController: _controller,
+              );
+            } else if (state is SentimentAnalyzerSuccess) {
+              return Center(
+                child: Text("Success: ${state.sentimentResponse.status}"),
+              );
+            } else if (state is SentimentAnalyzerLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is SentimentAnalyzerError) {
+              return Center(
+                child: Text(state.message),
+              );
+            } else {
+              return const Center(
+                child: Text("Something error"),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
